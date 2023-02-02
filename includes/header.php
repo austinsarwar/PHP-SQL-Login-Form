c4a0504d63e1dafd5ff99c59b4ebe5fca43f1054
austinsarwar <austin_sarwar@yahoo.com>
2023-02-01 23:28:40 -0500

    Setup Home and Login Page

--- includes/header.php
+++ includes/header.php
@@ -10,5 +10,16 @@ require_once "includes/database.php"
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="register.php">Register</a></li>
+                
+            </ul>
+        </nav>
+    </header>
